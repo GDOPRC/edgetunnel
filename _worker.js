bdc12c1c36dc96324323fc87b86b3dd0fa5fa0c4
GDOPRC <GDOPRC@TUTAMAIL.COM>
2024-10-11 16:49:05 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -11,7 +11,7 @@ let proxyIP = '';// 小白勿动，该地址并不影响你的网速，这是给
 let sub = '';// 避免项目被滥用，现已取消内置订阅器
 let subconverter = 'ProxyIP.Multacom.fxxk.dedyn.io';// clash订阅转换后端，目前使用CM的订阅转换功能。自带虚假uuid和host订阅。
 let subconfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini"; //订阅配置文件
-let subProtocol = 'https';
+let subProtocol = 'https://ProxyIP.Multacom.fxxk.dedyn.io';
 // The user name and password do not contain special characters
 // Setting the address will ignore proxyIP
 // Example:  user:pass@host:port  or  host:port
@@ -37,7 +37,7 @@ let go2Socks5s = [
 let addresses = [
 	//当sub为空时启用本地优选域名/优选IP，若不带端口号 TLS默认端口为443，#号后为备注别名
 	/*
-	'Join.my.Telegram.channel.1939.to.unlock.more.premium.nodes.link3.cc/1939#加入我的频道t.me/if1939解锁更多优选节点',
+	'Join.my.Telegram.channel.1939.to.unlock.more.premium.nodes.#加入我的频道t.me/if1939解锁更多优选节点',
 	'visa.cn:443',
 	'www.visa.com:8443',
 	'cis.visa.com:2053',
